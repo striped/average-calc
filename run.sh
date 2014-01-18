ad0ca586f2965defb5fdf9265337cbdaa3e02e1d
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2014-01-18 18:37:22 +0200

    add scala libraries to the classpath

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CP=scala-sma/target/classes:java-sma/target/classes:shared/target/classes
+CP=./scala-sma/target/classes:./java-sma/target/classes:./shared/target/classes:~/.m2/repository/org/scala-lang/scala-library/2.10.4-RC1/scala-library-2.10.4-RC1.jar:~/.m2/repository/org/scalaz/scalaz-concurrent_2.10/7.1.0-M4/scalaz-concurrent_2.10-7.1.0-M4.jar:~/.m2/repository/org/scalaz/scalaz-core_2.10/7.1.0-M4/scalaz-core_2.10-7.1.0-M4.jar
 
 JAVA_OPTS='-server -Xms2g -Xmx2g -XX:NewSize=1g -Xss228k -XX:PermSize=64m -XX:MaxPermSize=64m -XX:+TieredCompilation -XX:+UseG1GC -XX:+UseNUMA -XX:+UseCondCardMark -XX:-UseBiasedLocking -XX:+AlwaysPreTouch'
 
