e1940e21c7baa5678d1fb08b7894b81d2c90c889
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2014-01-18 17:12:21 +0200

    replace GetSum by GetAverage

--- scala-sma/src/test/scala/com/github/plokhotnyuk/sma/SMAAlgorithm2Test.scala
+++ scala-sma/src/test/scala/com/github/plokhotnyuk/sma/SMAAlgorithm2Test.scala
@@ -6,23 +6,20 @@ import org.junit.Test
 import org.kot.test.Algorithm
 import org.scalacheck.Prop
 import org.scalacheck.Prop._
+import org.scalacheck.Gen._
 import org.scalacheck.{Test => ScalacheckTest}
 import org.scalacheck.Test.Parameters
 
 class SMAAlgorithm2Test {
-  @Test def shouldCalcSumOfLastPrices(): Unit = check(Prop.forAll {
+  @Test def shouldCalcSumOfLastPrices(): Unit = check(forAll(nonEmptyListOf(choose(0, 999))) {
     (prices: List[Int]) =>
+      val lastPrices = prices.takeRight(Algorithm.DEPTH)
+      val expectedAverage = lastPrices.sum / lastPrices.size
       val completionFlag = new CountDownLatch(1)
       val actor = SMAAlgorithm2.createActor()
-      prices.foreach(price => actor ! Update(price))
-      var sum: Int = 0
-      actor ! GetSum {
-        (s: Int) =>
-          sum = s
-          completionFlag.countDown()
-      }
+      prices.foreach(p => actor ! Update(p))
+      actor ! GetAverage((a: Int) => if (a == expectedAverage) completionFlag.countDown())
       completionFlag.await(100, TimeUnit.MILLISECONDS)
-      sum == prices.takeRight(Algorithm.DEPTH).sum
   })
 
   private def check(p: Prop): Unit = assertTrue(ScalacheckTest.check(Parameters.defaultVerbose, p).passed)
